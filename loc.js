2b5071f116b8b3ec41b22509b3ce3a1c15278858
Jaimin <jaimin5.dev@gmail.com>
2024-02-28 18:17:18 +0530

    4th commit

--- loc.js
+++ loc.js
@@ -14,7 +14,7 @@ let showTemp = () => {
         let dt = new Date(data.dt * 1000);
         date.textContent = 'Updated as of ' + dt.toLocaleString();
 
-        //temp
+        //temp3
         let temp = Math.round(data.main.temp - 273.15);
         let weather = document.getElementById('weather');
         weather.textContent = temp + '°';
@@ -48,6 +48,12 @@ let showTemp = () => {
         let visibilityId = document.getElementById('visibilityId');
         let visibility = (data.visibility / 1000);
         visibilityId.textContent = 'Visibility ' + visibility + ' Km/h';
+
+        const cityNotFoundDiv = document.getElementById("cityNotFound");
+        cityNotFoundDiv.classList.add("hidden");
+        
+        const eee = document.getElementById("cityFound");
+        eee.classList.remove("hidden");
     }
 
     function updateforeCastWeatherUI(data) {
@@ -116,14 +122,19 @@ let showTemp = () => {
             .then(data => {
                 console.log("Weather Data:", data);
                 if (data.cod !== 200) {
-                    alert('city not found');
+                    const cityNotFoundDiv = document.getElementById("cityFound");
+                    cityNotFoundDiv.classList.add("hidden");
+                    const ddd = document.getElementById("cityNotFound");
+                    ddd.classList.remove("hidden");
                     // document.querySelector('.weather-details').innerHTML = "City not found";
                     // document.querySelector('.forecast').innerHTML = '';
+
+
                 } else {
+
                     // Call updateWeatherUI function to update the UI with weather data
                     document.getElementById('city').textContent = data.name;
                     document.getElementById('description').textContent = data.weather[0].description;
-                    console.log("Weather Data (Inside else):", data);
                     updateWeatherUI(data);
                 }
             })
@@ -140,49 +151,7 @@ let showTemp = () => {
             .then(response => response.json())
             .then(data => {
                 updateforeCastWeatherUI(data);
-                // const encounteredDates = {};
-                // let forecastIndex = 1;
-                // let forecastIconIndex = 1;
 
-                // for (let i = 1; i < data.list.length; i++) {
-                //     let dateis = new Date(data.list[i].dt * 1000);
-                //     let datenow = dateis.getDate();
-
-                //     if (datenow !== new Date().getDate() && !encounteredDates[datenow]) {
-                //         encounteredDates[datenow] = true;
-
-                //         const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-                //         let dayIndex = dateis.getDay();
-                //         let day1 = days[dayIndex];
-
-                //         let forecastDayDate = document.querySelector(`.forecastDay${forecastIndex}-date`);
-                //         forecastDayDate.textContent = day1 + ' ' + datenow;
-
-                //         let iconId = data.list[i].weather[0].icon;
-                //         let iconUrl = (`https://openweathermap.org/img/wn/${iconId}@2x.png`)
-                //         let forecastIcon = document.querySelector(`.forecastIcon${forecastIconIndex}`);
-                //         forecastIcon.src = iconUrl;
-
-                //         let temperature = document.querySelector(`.temperature${forecastIndex}`);
-                //         let tempData = Math.round(data.list[i].main.temp - 273);
-                //         temperature.textContent = tempData + '°';
-
-                //         let minTemp = document.querySelector(`.minTemp${forecastIndex}`);
-                //         let lowTemp = Math.round(data.list[i].main.temp_min - 273);
-                //         minTemp.textContent = lowTemp + '°';
-
-                //         let weatherDesc = document.querySelector(`.weather-desc${forecastIndex}`);
-                //         let wthrdesc = data.list[i].weather[0].description;
-                //         weatherDesc.textContent = wthrdesc;
-
-                //         forecastIndex++;
-                //         forecastIconIndex++;
-
-                //         if (forecastIndex && forecastIconIndex > 5) {
-                //             break;
-                //         }
-                //     }
-                // }
             })
             .catch(err => console.error(err))
     });
